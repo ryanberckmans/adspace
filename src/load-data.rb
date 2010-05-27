86652db9fbc98f854ad781cfffd22d9c73885227
Ryan Berckmans <ryan.berckmans@gmail.com>
2010-05-27 16:20:19 -0400

    0.9

--- src/load-data.rb
+++ src/load-data.rb
@@ -1,13 +1,21 @@
 
+
+def _valid( entry )
+  return false if entry.lstrip.length < 1
+  return false if entry =~ /\#/
+  
+  true
+end
+
 def _loadURLs( urlFile )
   urls = []
-  File.foreach( urlFile ) { |url| urls.push url unless url =~ /\#/ }
+  File.foreach( urlFile ) { |url| urls.push( url.chomp ) if _valid(url) }
   urls
 end
 
 def _loadScans( scanFile )
   scans = []
-  File.foreach( scanFile ) { |scan| scans.push( scan ) if scan.length > 1 and not (scan =~ /\#/) }
+  File.foreach( scanFile ) { |scan| scans.push( scan.chomp ) if _valid(scan) }
   scans
 end
 
