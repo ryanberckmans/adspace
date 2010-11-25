2dcee958c6cbaf13652d6bc16cc6e84eb18ce021
Ryan Berckmans <ryan.berckmans@gmail.com>
2010-11-25 10:49:08 -0500

    scheduler computes amount of scans to add; added coroutine library

--- lib/scheduler/base.rb
+++ lib/scheduler/base.rb
@@ -1,11 +1,29 @@
 require "core/util.rb"
 require Util.here "commandline-options.rb"
+require "core/coroutine.rb"
 
 module Scheduler
-  INTERVAL = 60 * 2
+  INTERVAL = 60 * 1
   DESIRED_INTERVALS = 4
   NEW_RATE_RATIO = 0.5
+  MINIMUM_QUEUE_SIZE = 20
 
+  def self.consumption_tracker( coroutine )
+    previous_size = 0
+    consumption_rate = 0.0
+    while true
+      previous_size = AWS::SQS::size rescue previous_size
+      coroutine.yield
+      size = AWS::SQS::size rescue previous_size
+      puts previous_size
+      puts size
+      consumption_rate = consumption_rate * NEW_RATE_RATIO + (previous_size - size) * (1 - NEW_RATE_RATIO)
+      puts "consumption_rate: #{consumption_rate}"
+      increase_queue_by = DESIRED_INTERVALS * [consumption_rate,0.0].max - size
+      coroutine.yield [increase_queue_by.to_i,0,MINIMUM_QUEUE_SIZE - size].max
+    end
+  end
+  
   def self.run
     options = Scheduler::parse_options ARGV
 
@@ -13,21 +31,13 @@ module Scheduler
     puts "using sqs queue #{$SQS_QUEUE}"
     require "core/sqs-interface.rb"
 
-    consumption_rate = 0.0
-    previous_size = 0
+    consumption = Coroutine.new { |cr| consumption_tracker cr }
     
     while true
-      previous_size = AWS::SQS::size rescue previous_size
+      consumption.resume
       sleep INTERVAL
-      size = AWS::SQS::size rescue previous_size
-      puts previous_size
-      puts size
-      consumption_rate = consumption_rate * NEW_RATE_RATIO + (previous_size - size) * (1 - NEW_RATE_RATIO)
-      puts "consumption_rate: #{consumption_rate}"
-      intervals_remaining = size / [consumption_rate,0.0].max
-      puts "intervals' worth of work remaining: #{intervals_remaining}"
-      puts "top-up required" if intervals_remaining < DESIRED_INTERVALS
-      # TOP UP
+      increase_queue_by = consumption.resume
+      puts "final increase queue by #{increase_queue_by}"
     end
   end
 end
